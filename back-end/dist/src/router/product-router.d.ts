16d509bd8c4f66d83477a98b1399f43bac168b86
DragonRF <duonglong410@gmail.com>
2023-05-08 13:47:45 +0700

    pulling

--- back-end/dist/src/router/product-router.d.ts
+++ back-end/dist/src/router/product-router.d.ts
@@ -1,1 +1,1 @@
-export declare const productRouter: import("express-serve-static-core").Router;
+export declare const productRouter: any;
--- back-end/dist/src/router/router.d.ts
+++ back-end/dist/src/router/router.d.ts
@@ -1,1 +1,1 @@
-export declare const router: import("express-serve-static-core").Router;
+export declare const router: any;
